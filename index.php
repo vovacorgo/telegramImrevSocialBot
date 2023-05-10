be63726d33e3f4a0d4f7f7a2b7ad37e8f1ac03dc
Volodymyr <47760619+vovacorgo@users.noreply.github.com>
2023-05-10 12:07:25 +0300

    First stable commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+    require __DIR__.'/vendor/autoload.php';
+
+
+use CustomCommands\StartCommand;
+use Telegram\Bot\Api;
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$telegramBotToken =  \Illuminate\Support\Arr::get(env('TELEGRAM_BOT_TOKEN'), null);;
+
+if (!$telegramBotToken) {
+    throw new Exception('TELEGRAM_BOT_TOKEN environment variable not set -'.__DIR__);
+}
+
+
+
+$telegram = new Api($telegramBotToken);
+    $result = $telegram->getWebhookUpdates();
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
+$name = $result["message"]["from"]["username"];
+$first_name = $result["message"]["from"]["first_name"];
+$last_name = $result["message"]["from"]["last_name"];
+$username = $first_name . ' ' . $last_name;
+
+$keyboard = [
+    ['New',],
+];
+
+$reply_markup = $telegram->replyKeyboardMarkup([
+    'keyboard' => $keyboard,
+    'resize_keyboard' => true,
+    'one_time_keyboard' => true
+]);
+
+$response = $telegram->sendMessage([
+    'chat_id' => $chat_id,
+    'text' => 'Buttons',
+    'reply_markup' => $reply_markup
+]);
+$telegram->addCommands([
+    Telegram\Bot\Commands\HelpCommand::class,
+
+]);
+$telegram->addCommand(StartCommand::class);
+$update = $telegram->commandsHandler(true);
+
+if($update->getMessage()->has('text'))
+{   $text = $update->getMessage()->getText();
+    switch ($text){
+        case 'New':
+            $message = "Вітаю";
+            $keyboard = [
+                ['Ліво', 'Право', 'Центер'],
+                ['Назад']
+
+            ];
+            break;
+    }
+    $reply_markup = $telegram->replyKeyboardMarkup([
+        'keyboard' => $keyboard,
+        'resize_keyboard' => true,
+        'one_time_keyboard' => true
+    ]);
+}
+
+function env(string $variable){
+    return $_ENV[$variable];
+}
